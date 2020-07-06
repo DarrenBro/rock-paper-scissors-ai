17d0c784b3ee7edcf3b7bc29b86c2906239b2fe5
Darren Broderick <darren.broderick@liberty-it.co.uk>
2020-07-06 17:41:53 +0100

    pseudocode rules for later on, getting dinner

---
+++ play.py
@@ -0,0 +1,13 @@
+# start with comp plays random move
+
+# human plays their move
+
+# comp sees images
+
+# based on image and rules for game plays the winning move
+
+# play move by test then maybe by generic image?
+
+# note, what if same move made, reset game or display something like draw
+
+# extra: counter for wins
