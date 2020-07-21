f4efd898b9bbd0966d14d14ad0e6f407ba61f331
Darren Broderick <darren.broderick@liberty-it.co.uk>
2020-07-21 10:52:03 +0100

    Play script updates

--- play.py
+++ play.py
@@ -48,7 +48,13 @@ def calculate_win_move(user_move):
 
 model = load_model("example-rps-model-1.h5")
 cap = cv2.VideoCapture(0)
+
 previous_move = None
+user_wins = 0
+janken_wins = 0
+round_count = 0
+janken_win_count = 0
+user_win_count = 0
 
 while True:
     # "frame" will get the next frame in the camera
@@ -86,10 +92,6 @@ while True:
     move_code = np.argmax(prediction[0])
     user_move_option = mapper(move_code)
 
-    round_count = 0
-    janken_win_count = 0
-    user_win_count = 0
-
     # determine winner
     if previous_move != user_move_option:
         if user_move_option != "none":
@@ -114,24 +116,19 @@ while True:
     cv2.putText(frame, "Janken's Move: " + janken_win_move,
                 (835, 60), font, 1, (255, 255, 255), 2, cv2.LINE_AA)
 
-    # icon = cv2.imread("win_images/unknown_win_image.png")
-    # icon = cv2.resize(icon, (50, 50))
-    # frame[600:650, 600:650] = icon
-
-    # icon = cv2.imread("win_images/janken_win_image.png")
-    # icon = cv2.resize(icon, (50, 50))
-    # frame[600:650, 600:650] = icon
-    #
-    # icon = cv2.imread("win_images/user_win_image.png")
-    # icon = cv2.resize(icon, (50, 50))
-    # frame[600:650, 600:650] = icon
+    # Debugger
+    # cv2.putText(frame, "Round count: " + str(round_count), (100, 450), font, 1, (255, 255, 255), 1,cv2.LINE_AA)
+    # cv2.putText(frame, "Janken win count: " + str(janken_win_count), (100, 480), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
+    # cv2.putText(frame, "User win count: " + str(user_win_count), (100, 510), font, 1, (255, 255, 255), 1,cv2.LINE_AA)
+    # cv2.putText(frame, "user_wins: " + str(user_wins), (100, 540), font, 1, (255, 255, 255), 1, cv2.LINE_AA)
 
     try:
         winner
     except NameError:
         print("well, winner WASN'T defined after all!")
     else:
-        if round_count == 0:
+        if winner == "Show me your move":
+            # if round_count == 0:
             cv2.putText(frame, "Best of 3",
                         (550, 600), font, 1.2, (0, 0, 255), 3, cv2.LINE_AA)
         elif winner == "Tie":
@@ -140,6 +137,30 @@ while True:
         else:
             cv2.putText(frame, "Winner is: " + winner,
                         (470, 600), font, 1.2, (0, 0, 255), 3, cv2.LINE_AA)
+
+    # if janken_win_count and user_win_count == 0:
+    icon = cv2.imread("win_images/unknown_win_image.png")
+    icon = cv2.resize(icon, (50, 50))
+
+    frame[640:690, 500:550] = icon
+    frame[640:690, 600:650] = icon
+    frame[640:690, 700:750] = icon
+
+    if user_wins:
+        icon = cv2.imread("win_images/user_win_image.png")
+        icon = cv2.resize(icon, (50, 50))
+
+        frame[640:690, 500:550] = icon
+        frame[640:690, 600:650] = icon
+        frame[640:690, 700:750] = icon
+
+        # icon = cv2.imread("win_images/janken_win_image.png")
+        # icon = cv2.resize(icon, (50, 50))
+        # frame[600:650, 600:650] = icon
+        #
+        # icon = cv2.imread("win_images/user_win_image.png")
+        # icon = cv2.resize(icon, (50, 50))
+        # frame[600:650, 600:650] = icon
 
     if janken_win_move != "none":
         icon = cv2.imread(
@@ -153,10 +174,19 @@ while True:
         frame[100:500, 800:1200] = icon
 
     # Title for panel (Needed for pyGame)
-    cv2.imshow("Place Hand Gesture In Empty Box", frame)
+    cv2.imshow("Play Hand Gesture In Frame", frame)
 
-    k = cv2.waitKey(10)
-    if k == ord('q'):
+    key = cv2.waitKey(10)
+    if key == ord('u'):
+        user_wins += 1
+
+    if key == ord('j'):
+        janken_wins += 1
+
+    if key == ord('p'):
+        cv2.waitKey(-1)  # wait until any key is pressed
+
+    if key == ord('q'):
         break
 
 cap.release()
